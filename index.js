3ba07ff62a22d94dcc743eda8820fd2591dbe3ee
Hola Bart <marcoferrusquia@gmail.com>
2023-08-01 19:59:45 -0600

    final details

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// TODO: Include packages needed for this application
+
 
 const fs = require("fs");
 const inquirer = require("inquirer");
@@ -6,7 +6,7 @@ const path = require("path");
 const generateMarkdown=require("./utils/generateMarkdown")
 
 
-// TODO: Create an array of questions for user input
+
 const questions = [
     {
       type: "input",
@@ -77,7 +77,7 @@ const questions = [
   ];
   
 
-// TODO: Create a function to write README file
+
 function writeToFile(fileName, data) {
     fs.writeFile(fileName, data, (err)=>{
         if (err){
@@ -88,7 +88,7 @@ function writeToFile(fileName, data) {
     });
 }
 
-// TODO: Create a function to initialize app
+
 function init() {
     inquirer.prompt(questions).then((answers)=>{
         const markdownContent=generateMarkdown(answers);
@@ -96,5 +96,5 @@ function init() {
     })
 }
 
-// Function call to initialize app
+
 init();
